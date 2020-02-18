efccfa89be1e8b8098db22fb8555592db5ae344c
Arpan Dhatt <ary.dhatt@gmail.com>
2020-02-18 16:44:42 -0600

    worked on hangman

--- src/hangman.rs
+++ src/hangman.rs
@@ -1,10 +1,10 @@
 use std::fs;
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet};
 
 pub struct Hangman {
     word_length: usize,
     total_guesses: usize,
-    letters_guessed: Vec<char>,
+    letters_guessed: BTreeSet<char>,
     words: Vec<String>,
 }
 
@@ -14,32 +14,60 @@ impl Hangman {
             .expect("Failed to read dictionary!")
             .split("\n")
             .filter(|word| word.len()==word_length)
-            .map(|word| String::from(word))
+            .map(|word| String::from(word).trim().to_string())
             .collect();
 
         Hangman {
             word_length: word_length,
             total_guesses: total_guesses,
-            letters_guessed: vec![],
+            letters_guessed: BTreeSet::new(),
             words: words
         }
     }
 
     pub fn guess(&mut self, guess: char) {
+        if self.letters_guessed.contains(&guess) {
+            return
+        }
+        let mut families = self.get_families(guess);
+        self.letters_guessed.insert(guess);
+
+        let mut biggest = families.get(families.keys().next().unwrap()).unwrap();
+        for (k,v) in families.iter() {
+           if v.len() > biggest.len() {
+               biggest = v;
+           }
+        }
+        self.words = biggest.clone();
+        println!("{:?}",self.words);
 
     }
 
-    fn get_families(&self, words: Vec<String>, contains: char) {
-        let mut contains_char = words
+    fn get_families(&self, contains: char) -> BTreeMap<String,Vec<String>> {
+        let mut contains_char = self.words
             .iter()
             .filter(|word| word.contains(contains));
-        let families: BTreeMap<String,Vec<String>> = BTreeMap::new();
+        let mut families: BTreeMap<String,Vec<String>> = BTreeMap::new();
         for word in contains_char {
-
+            let family = Hangman::family_key(&word, contains);
+            if families.contains_key(&family) {
+                families.get_mut(&family).unwrap().push(word.clone());
+            } else {
+               families.insert(family,vec![word.clone()]);
+            }
         }
+        families
     }
 
     fn family_key(word: &String, character: char) -> String {
-
+        let mut out = String::new();
+        for c in word.chars() {
+            if c == character {
+                out.push(c);
+            } else {
+                out.push('_');
+            }
+        }
+        out
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
+use std::io;
+use std::io::{Read, Write};
+use crate::hangman::Hangman;
+
 mod hangman;
 
 fn main() {
+    let mut buf = String::new();
+    println!("How many guesses? ");
+    io::stdin().read_line(&mut buf)
+        .expect("error reading input for 'How many guesses?'");
+
+    let n_guesses: usize = buf.trim().parse()
+        .expect("couldn't parse number of guesses");
+
+    buf = String::new();
+    println!("How long will the word be? ");
+    io::stdin().read_line(&mut buf)
+        .expect("error reading input for 'How long will the word be?'");
+    let n_size: usize = buf.trim().parse()
+        .expect("couldn't parse word length");
+
+    let mut hg = Hangman::new(&String::from("words_alpha.txt"),n_size, n_guesses);
+
+    hg.guess('c');
 }
